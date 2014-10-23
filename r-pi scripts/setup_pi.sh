01b1b2e181203749639beb09b9443b7ceeb11503
d1390 <Diego Trujillo>
2014-10-23 10:52:58 +0100

    Made setup and initialization scripts more verbose and corrected an error in the dhcp range

--- r-pi scripts/setup_pi.sh
+++ r-pi scripts/setup_pi.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # RUN THIS AS SUDO
-
+echo "Installing and updating packages"
 apt-get install aptitude
 aptitude update; aptitude safe-upgrade
 aptitude install rfkil zd1211-firmware hostapd hostap-utils iw dnsmasq
+echo "Copying configuration files"
 mkdir ~/backups
 cp /etc/network/interfaces ~/backups/interfaces.backup
 cp /etc/dnsmasq.conf ~/backups/dnsmasq.conf.backup
@@ -11,9 +12,11 @@ cp ./interfaces /etc/network/interfaces
 cp ./hostapd.conf /etc/hostapd/hostapd.conf
 hostapd -B /etc/hostapd/hostapd.conf
 cp ./dnsmasq.conf /etc/dnsmasq.conf
+"Starting access point"
 service dnsmasq restart
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -j MASQUERADE
+"Seting up access point on boot"
 cp ./pipoint /etc/init.d/pipoint
 chmod +x /etc/init.d/pipoint
 update-rc.d pipoint start 99
