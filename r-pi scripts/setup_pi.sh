2187f4632c0c3cc2ffc6968b62a6bc8e6db32628
d1390 <Diego Trujillo>
2014-10-23 10:35:04 +0100

    Added an initialization script that will start the R-Pi as an access point on boot. Modified setup-pi to install this script

--- r-pi scripts/setup_pi.sh
+++ r-pi scripts/setup_pi.sh
@@ -1,4 +1,4 @@
-#!/bin/s
+#!/bin/sh
 # RUN THIS AS SUDO
 
 apt-get install aptitude
@@ -10,7 +10,10 @@ cp /etc/dnsmasq.conf ~/backups/dnsmasq.conf.backup
 cp ./interfaces /etc/network/interfaces
 cp ./hostapd.conf /etc/hostapd/hostapd.conf
 hostapd -B /etc/hostapd/hostapd.conf
-cp ./dnsmasq.conf /etc/
+cp ./dnsmasq.conf /etc/dnsmasq.conf
 service dnsmasq restart
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -j MASQUERADE
+cp ./pipoint /etc/init.d/pipoint
+chmod +x /etc/init.d/pipoint
+update-rc.d pipoint start 99
