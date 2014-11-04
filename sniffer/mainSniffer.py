41b2c990345c60bc605b2461d05491d7c5f26745
d1390 <Diego Trujillo>
2014-11-04 09:35:51 +0000

    Made several minar changes to the way functions work. Added a quick analysis function that uses only DNS qname to determin services.

--- sniffer/mainSniffer.py
+++ sniffer/mainSniffer.py
@@ -1,28 +1,50 @@
 #!/bin/python
-#usage:sniff(iface='wlan0',prn=analysis)
-
+#usage:
+# Full analysis: sniff(iface='wlan0',prn=analysis,store=0)
+# Quick analysis: sniff(iface='wlan0',prn=quickAnalysis,store=0)
 from scapy.all import *
 import datetime
 import json
 import socket
 from ipwhois import IPWhois,IPDefinedError
+from uuid import getnode as get_mac
+import re
 
+ip_regex=re.compile('\d*[.]\d*[.]\d*[.]\d*')
 mac_useragent_map={}
 ip_domain_map={'local':'192.168.0'}
 start_time=time.time()
-services=['facebook','twitter','flickr','instagram','whatsapp','skype','youtube','fbcdn','fbstatic','twvid','twimg']
+services={'facebook':'facebook','fbcdn':'facebook','fbstatic':'facebook','twitter':'twitter','twvid':'twitter','twimg':'twitter','flickr':'flickr','instagram':'instagram','whatsapp':'whatsapp','softlayer':'whatsapp','skype':'skype','youtube.com':'youtube','googlevideo':'youtube','ytimg':'youtube'}
 
 def timestamp():
-	'''Creates a formatted timestap string using the format 
-	yearMonthDay Hour:Minute:Second.
+	'''Creates a formatted timestap string using unix time
+        format in milliseconds (JS friendly)
 	Takes:
 	Nothing
 	Returns:
 	String
 	'''
-	dt =datetime.datetime.now()
-	return dt.strftime('%y%m%d '+'%I:%M:%S')
-	
+	dt =datetime.datetime.utcnow()
+        epoch=datetime.datetime.utcfromtimestamp(0)
+        delta = dt-epoch
+        return delta.total_seconds()*1000
+def getRouterMac():
+        '''Gets the router's mac address as a Long number,
+        conerts it to hex and formats it to a familiar 
+        represantation.
+        Takes:
+        Nothing
+        Returns:
+        macaddr->str
+        '''
+	macaddr=''
+        mac=get_mac()
+        mac=hex(mac)
+        for i in range(2,len(mac)-1,2):
+                macaddr+=mac[i]+mac[i+1]+':'
+        macaddr=macaddr[0:-1]
+        return macaddr 
+
 def getHostname(ip):
 	'''Attempts to get a packet's destination's host name 
 	using socket's get host by address.
@@ -185,7 +207,7 @@ def serviceInWhois(whoIs,service,key=''):
         return False
                                                         
                                         
-def submitReport(server,report): #TODO
+def submitReport(server,report): #TODO: Publish to php
 	'''Writes a json packet report to the specified 
 	server.
 	Takes:
@@ -194,10 +216,93 @@ def submitReport(server,report): #TODO
 	Returns:
 	Nothing
 	'''
-	pass
-def analysis(packet): #TODO This is an ugly function, split it into several funcitons!
+        #FOR THE TIME BEING WRITES TO FILE
+        with open(server,'a') as outFile:
+                outFile.write(report+',')
+                
+def quickAnalysis(packet):
+        '''
+	Runs only a qname and useragent analysis of the packet, converts the results to
+	JSON and posts it to the server. It ignores packages with the router as source.
+        The report takes the format {timestamp,deviceAddress,packetId,domain,<userAgent> 
+        (if available)}
+        '''
+	proceed=0
+	#<start information gathering>
+	time=timestamp()
+	deviceAddress=packet.src
+        routerMac='c0:4a:00:1e:42:76'
+        remoteHost='local'
+        rawLoad=getRawLoad(packet)# Attempt to get a Raw load.
+        if packet.haslayer(IP): #Does our package have an IP layer?
+                packetId=packet[IP].id
+                if '192.168.0' not in packet[IP].src: # Is the source or destination remote?
+                        remoteHost=packet[IP].src
+                elif '192.168.0' not in packet[IP].dst:
+                        remoteHost=packet[IP].dst
+                else:
+                        remoteHost='local'
+        else:
+                packetId=None
+        qname=getQname(packet)# Attempt to get DNSQR qname
+        if deviceAddress != routerMac:
+                #Is our service in qname?
+                if qname != None:
+                        for s in services.keys():
+                                regEx=re.compile('.*'+s+'.*')
+                                if regEx.match(qname) != None:
+                                        print regEx.match(qname).group() + " in qname"
+                                        domain=services[s]
+                                        proceed = 1
+                                        break
+                else:
+                        domain=remoteHost
+                        proceed=0
+        else:
+                proceed=0
+
+        if rawLoad != None:
+                #Do we have a user agent on record?
+                if deviceAddress not in mac_useragent_map.keys():
+                        if deviceAddress == routerMac:
+                                mac_useragent_map[deviceAddress]='rPi-router'
+                        else:
+                                userAgent=getUserAgent(rawLoad)
+                                mac_useragent_map[deviceAddress]=userAgent
+                elif mac_useragent_map[deviceAddress]==None:
+                        userAgent=getUserAgent(rawLoad)
+                        mac_useragent_map[deviceAddress]=userAgent
+                else:
+                        userAgent=mac_useragent_map[deviceAddress]
+                        #<end information gathering>
+                        #<start report packing>
+                        if proceed==1:
+                                packetData={'time':time,'HWaddr':deviceAddress,'pId':packetId,'domain':domain,'user-agent':userAgent}
+                                jsonisedData=json.dumps(packetData)
+                                submitReport('relevant.json',jsonisedData)
+                                return jsonisedData
+                        else:
+                                return qname
+        else:
+                if proceed==1:
+                        if deviceAddress in mac_useragent_map.keys():
+                                userAgent=mac_useragent_map[deviceAddress]
+                                packetData={'time':time,'HWaddr':deviceAddress,'pId':packetId,'domain':domain,'user-agent':userAgent}
+                                jsonisedData=json.dumps(packetData)
+                                submitReport('relevant.json',jsonisedData)
+                                return jsonisedData
+                                                
+                        else:
+                                packetData={'time':time,'HWaddr':deviceAddress,'pId':packetId,'domain':domain,'user-agent':''}
+                                jsonisedData=json.dumps(packetData)
+                                submitReport('relevant.json',jsonisedData)
+                                return jsonisedData
+                else:
+                        return qname
+
+def analysis(packet):
 	'''
-	Runs a full analysis of the specified package, converts
+	Runs a full analysis of the package, converts
 	it JSON and posts it to the server. The report takes the format
 	{timestamp,deviceAddress,packetId,domain,<userAgent> (if available)}
 	'''
@@ -205,6 +310,7 @@ def analysis(packet): #TODO This is an ugly function, split it into several func
 	#<start information gathering>
 	time=timestamp()
 	deviceAddress=packet.src
+        routerMac='c0:4a:00:1e:42:76'
         remoteHost='local'
         rawLoad=getRawLoad(packet)# Attempt to get a Raw load.
         if packet.haslayer(IP): #Does our package have an IP layer?
@@ -222,20 +328,20 @@ def analysis(packet): #TODO This is an ugly function, split it into several func
                 print "Generating ip-domain item"
                 #Is our service in qname?
                 if qname != None:
-                        for s in services:
+                        for s in services.keys():
                                 if s in qname:
                                         print s + " in qname"
-                                        domain=s
-                                        ip_domain_map[remoteHost]=s+'.com'
+                                        domain=services[s]
+                                        ip_domain_map[remoteHost]=services[s]
                                         proceed = 1
                                         break
                 elif rawLoad != None: #Is it in our raw load?
                         if proceed==0:
-                                for s in services:
+                                for s in services.keys():
                                         if s in rawLoad:
                                                 print s + " in rawLoad"
-                                                domain=s
-                                                ip_domain_map[remoteHost]=s+'.com'
+                                                domain=services[s]
+                                                ip_domain_map[remoteHost]=services[s]
                                                 proceed = 1
                                                 break
                 else:
@@ -243,22 +349,25 @@ def analysis(packet): #TODO This is an ugly function, split it into several func
 
                 if proceed==0: 
                         hostname=getHostname(remoteHost)# or is it in a hostname
-                        for s in services:
+                        for s in services.keys():
                                 if s in hostname:
                                         print s + " in hostname"
-                                        domain=s
-                                        ip_domain_map[remoteHost]=s+'.com'
+                                        domain=services[s]
+                                        ip_domain_map[remoteHost]=services[s]
                                         proceed = 1
                                         break
                 if proceed==0:
                         whois=getWhois(remoteHost)# or do we need whois information?
-                        for s in services:
-                                if serviceInWhois(whois,s):
-                                        print s + " in whois"
-                                        domain=s
-                                        ip_domain_map[remoteHost]=s+'.com'
-                                        proceed = 1
-                                        break
+                        if whois != None:
+                                for s in services.keys():
+                                        if serviceInWhois(whois,s):
+                                                print s + " in whois"
+                                                domain=services[s]
+                                                ip_domain_map[remoteHost]=services[s]
+                                                proceed = 1
+                                                break
+                        else:
+                                pass
                 if proceed==0:
                         ip_domain_map[remoteHost]=remoteHost
                         print "Irrelevant service"
@@ -268,18 +377,37 @@ def analysis(packet): #TODO This is an ugly function, split it into several func
                 domain=ip_domain_map[remoteHost]
                 if remoteHost == 'local':
                         proceed=0
-                        return False
+                        return 'local'
                 elif domain != remoteHost:
                         print "In dictionary: "+remoteHost
                         proceed=1
                 else:
-                        pass
+                        if packet.haslayer(DNSQR):
+                                qname=getQname(packet)# Attempt to get DNSQR qname
+                                print "Re-evaluating irrelevant"
+                                #Is our service in qname?
+                                if qname != None:
+                                        for s in services.keys():
+                                                if s in qname:
+                                                        print s + " in qname"
+                                                        domain=services[s]
+                                                        ip_domain_map[remoteHost]=services[s]
+                                                        proceed = 1
+                                                        break
+                                else:
+                                        proceed=0
+                        else:
+                                proceed=0
+                                
         
         if rawLoad != None:
                 #Do we have a user agent on record?
                 if deviceAddress not in mac_useragent_map.keys():
-                        userAgent=getUserAgent(rawLoad)
-                        mac_useragent_map[deviceAddress]=userAgent
+                        if deviceAddress == routerMac:
+                                mac_useragent_map[deviceAddress]='rPi-router'
+                        else:
+                                userAgent=getUserAgent(rawLoad)
+                                mac_useragent_map[deviceAddress]=userAgent
                 elif mac_useragent_map[deviceAddress]==None:
                         userAgent=getUserAgent(rawLoad)
                         mac_useragent_map[deviceAddress]=userAgent
@@ -290,20 +418,24 @@ def analysis(packet): #TODO This is an ugly function, split it into several func
                         if proceed==1:
                                 packetData={'time':time,'HWaddr':deviceAddress,'pId':packetId,'domain':domain,'user-agent':userAgent}
                                 jsonisedData=json.dumps(packetData)
+                                submitReport('relevant.json',jsonisedData)
                                 return jsonisedData
                         else:
-                                return False
+                                return remoteHost
         else:
                 if proceed==1:
                         if deviceAddress in mac_useragent_map.keys():
                                 userAgent=mac_useragent_map[deviceAddress]
                                 packetData={'time':time,'HWaddr':deviceAddress,'pId':packetId,'domain':domain,'user-agent':userAgent}
                                 jsonisedData=json.dumps(packetData)
+                                submitReport('relevant.json',jsonisedData)
                                 return jsonisedData
                                                 
                         else:
                                 packetData={'time':time,'HWaddr':deviceAddress,'pId':packetId,'domain':domain,'user-agent':''}
                                 jsonisedData=json.dumps(packetData)
+                                submitReport('relevant.json',jsonisedData)
                                 return jsonisedData
                 else:
-                        return False
+                        return remoteHost
+
